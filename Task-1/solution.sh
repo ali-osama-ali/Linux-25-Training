32fb57234fd22e2396e682540fed03d480341a2a
ali-osama-ali <20201700502@cis.asu.edu.eg>
2025-08-05 22:15:25 +0300

    Task submission.

---
+++ Task-1/solution.sh
@@ -0,0 +1,10 @@
+pwd
+ls
+rm /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/trap
+cp /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/key /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2 
+mv /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/closed /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/open
+mv /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/open /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3
+rm /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/trap
+rm -r /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/gate-4/dimension-y/ /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/gate-4/dimension-x
+mkdir /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/gate-4/earth/home
+touch /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/gate-4/earth/home/bed
