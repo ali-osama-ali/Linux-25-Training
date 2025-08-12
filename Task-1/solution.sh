368185059e5838e753155bcc9e252038e490f0b6
ali-osama-ali <20201700502@cis.asu.edu.eg>
2025-08-12 23:42:34 +0300

    Task submission.

--- Task-1/solution.sh
+++ Task-1/solution.sh
@@ -1,10 +1,16 @@
 pwd
 ls
-rm /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/trap
-cp /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/key /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2 
-mv /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/closed /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/open
-mv /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/open /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3
-rm /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/trap
-rm -r /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/gate-4/dimension-y/ /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/gate-4/dimension-x
-mkdir /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/gate-4/earth/home
-touch /home/ali/osc-summer-training/Linux-25-Training/Task-1/gate-1/gate-2/gate-3/gate-4/earth/home/bed
+cd gate-1
+rm trap
+cp key gate-2
+cd gate-2
+mv closed open
+mv open gate-3
+cd gate-3
+rm trap
+cd gate-4
+rm -rf dimension-x dimension-y
+cd earth
+mkdir home
+cd home
+touch bed
