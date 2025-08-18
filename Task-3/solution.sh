e282ec72c265895cd7330ba3e69efe59b54ba0df
ali-osama-ali <20201700502@cis.asu.edu.eg>
2025-08-18 00:57:55 +0000

    Task submission.

--- Task-3/solution.sh
+++ Task-3/solution.sh
@@ -1,17 +1,17 @@
-useradd hero
-useradd intruder
-userdel intruder
-groupadd warriors
-groupadd healers
-usermod -aG warriors hero
-useradd saga
-usermod -aG healers saga
+sudo useradd hero
+sudo useradd intruder
+sudo userdel intruder
+sudo groupadd warriors
+sudo groupadd healers
+sudo usermod -aG warriors hero
+sudo useradd saga
+sudo usermod -aG healers saga
 touch treasure.txt
 echo "CyberWorld's Core" >> treasure.txt
-chmod 000 treasure.txt
-chown treasure.txt hero
-chmod u+rw treasure.txt
-sleep 100 &
-ps
-kill -19 3857
+sudo chmod 000 treasure.txt
+sudo chown treasure.txt hero
+sduo chmod u+rw treasure.txt
+sudo sleep 100 &
+sudo ps
+sudo kill -19 3857
 
